b15338431ad6fd8b589c9fc51b0f34cad9b38dad
Arvin Josol <arvin.josol@live.com>
2023-11-29 18:27:23 -0800

    move CORS statement up. fix link to video images.

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ const express = require("express")
 const cors = require("cors")
 const app = express()
 
+const { PORT } = process.env
+
+app.use(cors())
+
 const videosRoutes = require("./routes/videos")
 app.use("/videos", videosRoutes)
 
 app.use(express.static("public"))
-
-const { PORT } = process.env
-
-app.use(cors())
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`)
